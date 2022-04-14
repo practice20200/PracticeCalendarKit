30e0c3ef7f12783cd1d9aafdc02ffc78a6797176
practice20200 <wor9hard2018@gmail.com>
2022-04-13 17:41:36 -0700

    Modified AddedViewController'

--- CalendarPracticeCalendarKit/Controller/AddViewController.swift
+++ CalendarPracticeCalendarKit/Controller/AddViewController.swift
@@ -28,18 +28,14 @@ class AddViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let saveBTN = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveHandler))
+        navigationItem.rightBarButtonItem = saveBTN
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func saveHandler(){
+        
     }
-    */
+    
 
 }
