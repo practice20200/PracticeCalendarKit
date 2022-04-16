f22359aa440eba904eb5f64128a520bb2f89146d
practice20200 <wor9hard2018@gmail.com>
2022-04-15 19:55:26 -0700

    Added event edit function'

--- CalendarPracticeCalendarKit/Controller/EventKitViewController.swift
+++ CalendarPracticeCalendarKit/Controller/EventKitViewController.swift
@@ -11,7 +11,7 @@ import EventKitUI
 
 class EventKitViewController : UIViewController {
 
-    
+    let store = EKEventStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +23,33 @@ class EventKitViewController : UIViewController {
     }
     
     @objc func addHandler(){
-        let vc = EKEventViewController()
-        vc.delegate = self
-        present(vc,animated: true)
+        store.requestAccess(to: .event) { [weak self] success, error in
+            if success, error == nil {
+                DispatchQueue.main.async {
+                    guard let store = self?.store else { return }
+                    let newEvent = EKEventEditViewController()
+                    newEvent.eventStore = store
+                    self?.present(newEvent, animated: true)
+                    
+                    
+                    
+                    let vc = EKEventViewController()
+                    vc.delegate = self
+//                    vc.event = nil
+                    self?.present(vc,animated: true)
+                }
+            }
+        }
     }
-    
-
 }
 
 
 extension EventKitViewController : EKEventViewDelegate{
     func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
-        
+        controller.dismiss(animated: true)
     }
+    
+    
     
     
 }
